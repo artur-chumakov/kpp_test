a0cb4fe5f908b2de55e3edbe918f6061ca79a936
artur-chumakov <artur2004ch@gmail.com>
2024-08-13 19:29:39 +0200

    routing and languages commit

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  public selected_lang = 'EN';
+  public header_menu_home: string = "HOME";
+  public header_menu_news: string = "NEWS";
+  public header_menu_contacts: string = "CONTACTS";
 
+
+  onChange(value: any) {
+    this.selected_lang = value.target.value;
+    console.log(`language was set to ${this.selected_lang}`)
+    if (this.selected_lang === "EN") {
+      this.header_menu_home = "HOME";
+      this.header_menu_news = "NEWS";
+      this.header_menu_contacts = "CONTACTS";
+    } else if (this.selected_lang === "UA") {
+      this.header_menu_home = "HOLOVNA";
+      this.header_menu_news = "NOVYNY";
+      this.header_menu_contacts = "KONTAKTY";
+    } else if (this.selected_lang === "DE") {
+      this.header_menu_home = "HAUPTSEITE";
+      this.header_menu_news = "NACHRICHTEN";
+      this.header_menu_contacts = "KONTAKTEN";
+    }
+
+  }
 }
