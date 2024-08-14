32afe3ab8482f38433a908cfd64e762cb511062c
Serg Chumakov <nexans@ua.fm>
2024-08-14 23:19:52 +0300

    Lang enum & class

--- src/header/header.component.ts
+++ src/header/header.component.ts
@@ -1,35 +1,59 @@
 import {Component} from '@angular/core';
+import {KeyValuePipe} from "@angular/common";
+import {FormsModule} from "@angular/forms";
+
+enum Langs {
+  de = 'DE',
+  en = 'EN',
+  ua = 'UA'
+}
+
+export class Lang {
+
+  home: string;
+  news: string;
+  contact: string;
+
+  constructor(home: string, news: string, contact: string) {
+    this.home = home;
+    this.news = news;
+    this.contact = contact;
+  }
+}
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    KeyValuePipe,
+    FormsModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  public selected_lang = 'EN';
-  public header_menu_home: string = "HOME";
-  public header_menu_news: string = "NEWS";
-  public header_menu_contacts: string = "CONTACTS";
 
+  _langsMap = new Map()
+    .set(Langs.en, new Lang("Home", "News", "Contact"))
+    .set(Langs.de, new Lang("HAUPTSEITE", "NACHRICHTEN", "KONTAKTEN"))
+    .set(Langs.ua, new Lang("Домашня", "Новини", "Контакти"));
+
+  public selected_lang = Langs.en;
+  public header_menu_home: string = this._langsMap.get(Langs.en).home;
+  public header_menu_news: string = this._langsMap.get(Langs.en).news;
+  public header_menu_contacts: string = this._langsMap.get(Langs.en).contact;
 
   onChange(value: any) {
     this.selected_lang = value.target.value;
     console.log(`language was set to ${this.selected_lang}`)
-    if (this.selected_lang === "EN") {
-      this.header_menu_home = "HOME";
-      this.header_menu_news = "NEWS";
-      this.header_menu_contacts = "CONTACTS";
-    } else if (this.selected_lang === "UA") {
-      this.header_menu_home = "HOLOVNA";
-      this.header_menu_news = "NOVYNY";
-      this.header_menu_contacts = "KONTAKTY";
-    } else if (this.selected_lang === "DE") {
-      this.header_menu_home = "HAUPTSEITE";
-      this.header_menu_news = "NACHRICHTEN";
-      this.header_menu_contacts = "KONTAKTEN";
-    }
-
+    Object.values(Langs).forEach((value) => {
+      if (this.selected_lang === value) {
+        this.header_menu_home = this._langsMap.get(value).home;
+        this.header_menu_news = this._langsMap.get(value).news;
+        this.header_menu_contacts = this._langsMap.get(value).contact;
+      }
+    })
   }
+
+  protected readonly aLangs = Langs;
 }
